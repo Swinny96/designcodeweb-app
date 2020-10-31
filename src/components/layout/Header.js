f829abf45344d3a7a173daaba1eb1c193af7c450
Christopher Swinton <chris@theswintons.co.uk>
2020-10-31 16:34:20 +0000

    session 11 complete

---
+++ src/components/layout/Header.js
@@ -0,0 +1,39 @@
+import { Link } from 'gatsby'
+import React from 'react'
+
+const menuData = [
+    { title: "Courses", 
+      icon: "/images/icons/courses.svg", 
+      link: "/courses" 
+    },
+    {
+      title: "Tutorials",
+      icon: "/images/icons/tutorials.svg",
+      link: "/tutorials",
+    },
+    { title: "Pricing", 
+      icon: "/images/icons/pricing.svg", 
+      link: "/pricing" 
+    },
+    { title: "Search", 
+      icon: "/images/icons/search.svg", 
+      link: "/search" 
+    },
+    { title: "Account", 
+      icon: "/images/icons/account.svg", 
+      link: "/account" 
+    },
+  ]
+
+export default function Header() {
+    return (
+        <>
+        {menuData.map((item, index) => (
+            <Link to={item.link} key={index}>
+            <img src={item.icon} alt={item.title} />
+            {item.title}
+            </Link>
+        ))}
+        </>
+    )
+}
